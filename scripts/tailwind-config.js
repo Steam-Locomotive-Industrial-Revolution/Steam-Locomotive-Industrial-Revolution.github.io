9b381bfc2c89a0505d89496a257249b0aebe1017
Joe Bloggs <140453332+MySpace-2468@users.noreply.github.com>
2025-08-22 10:00:15 +1000

    Create tailwind-config.js

---
+++ scripts/tailwind-config.js
@@ -0,0 +1,14 @@
+// Extend Tailwind with your custom colors
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'yinmn-blue': '#4c6085',
+        'celestial-blue': '#39a0ed',
+        'aquamarine': '#36f1cd',
+        'mint': '#13c4a3',
+        'black-olive': '#32322c',
+      }
+    }
+  }
+}
